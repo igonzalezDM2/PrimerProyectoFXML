5a1c6ff7a09e71b39bbeddb750a247a924e17c92
Iker Gonzalez <nintzatekeen@gmail.com>
2023-09-28 09:27:21 +0200

    Funcionando

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,28 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("/fxml/CalculadoraFXML.fxml"));
+            Scene scene = new Scene(root,600,600);
+            scene.getStylesheets().add(getClass() .getResource("/css/application.css").toExternalForm());
+            primaryStage.setTitle("CALCULADORA");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/controllers/CalculadoraController.java
+++ src/controllers/CalculadoraController.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import java.util.Locale;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
@@ -30,6 +33,26 @@ public class CalculadoraController {
     	if (operador1str != null && !operador1str.isBlank() && operador2str != null && !operador2str.isBlank()) {
     		try {
     			Double operando1 = Double.parseDouble(operador1str);
+    			Double operando2 = Double.parseDouble(operador2str);
+    			
+    			RadioButton botonSeleccionado = (RadioButton) operaciones.getSelectedToggle();
+    			final String btnTxt = botonSeleccionado.getText().toLowerCase();
+    			Double resultado = 0d;
+    			switch (btnTxt) {
+				case "sumar": 
+					resultado = operando1 + operando2;
+					break;
+				case "restar":
+					resultado = operando1 - operando2;
+					break;
+				case "multiplicar":
+					resultado = operando1 * operando2;
+					break;
+				case "dividir":
+					resultado = operando1 / operando2;
+					break;
+				}    			
+    			txtResultado.setText(String.format(Locale.GERMAN, "%,.2f", resultado));
     		} catch (NumberFormatException e) {
     			txtResultado.setText("Operandos incorrectos");
     		} catch (ArithmeticException e) {
